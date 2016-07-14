b0828897ef29b49a441d7d148a6c715d6af109f2
Philip Huysmans <philip.huysmans@ua.ac.be>
2016-07-14 18:11:31 +0200

    initial commit

---
+++ routes/prRoutes.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var validator = require('../../prParser');
+
+router.get('/', function(req, res, next) {
+  res.render('upload', { title: 'PRMV' });
+});
+
+router.post('/upload', function(req, res) {
+	var modelFile;
+ 
+	if (!req.files) {
+		res.send('No model file was uploaded.');
+		return;
+	}
+
+	var report = validator.validate(req.files.prModel.data.toString());
+	res.render('report', {report: report});
+});
+
+
+module.exports = router;
